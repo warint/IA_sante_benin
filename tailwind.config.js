1b12937e4ab4252c4b935a95dba23cc400aa8a79
Doris-LeARN <jospine-doris.abadassi@epitech.eu>
2024-10-03 09:26:53 +0100

    Configuration et initialisation de tailwind css pour le design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./**/app.R",
+    "./www/**/*.html",
+    "./www/**/*.js",
+    "./www/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
